fa39bd6e3d55a433fa59a410e0687f480ce77282
Béa <beatrice.becco@gmail.com>
2024-05-06 15:59:11 +0200

    fetch API TMDB

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+
+//GET MOVIES FROM TMDB API
+router.get('/movies', (req, res) => {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      res.json({ movies: data.results })
+    })
+    .catch(err => console.error(err))
+})
+
+module.exports = router;
